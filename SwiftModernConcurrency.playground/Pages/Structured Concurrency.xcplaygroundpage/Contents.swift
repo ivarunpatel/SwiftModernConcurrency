d3f6d681416d17d1fec3fe181cfe8de36cfa5211
Varun Patel <varun@thepeoplemesh.com>
2023-05-10 10:10:29 +0530

    Fix demo

--- SwiftModernConcurrency.playground/Pages/Structured Concurrency.xcplaygroundpage/Contents.swift
+++ SwiftModernConcurrency.playground/Pages/Structured Concurrency.xcplaygroundpage/Contents.swift
@@ -21,7 +21,7 @@ enum ImageDownloadError: Error {
 
 func downloadImage(imageNumber: Int) async throws -> UIImage {
     try Task.checkCancellation()
-    let imageUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).png")!
+    let imageUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part3/\(imageNumber).png")!
     let imageUrlRequest = URLRequest(url: imageUrl)
     let (imageData, imageResponse) = try await URLSession.shared.data(for: imageUrlRequest)
     guard (imageResponse as? HTTPURLResponse)?.statusCode == 200,
@@ -33,7 +33,7 @@ func downloadImage(imageNumber: Int) async throws -> UIImage {
 
 func downloadMetadata(imageNumber: Int) async throws -> ImageMetadata {
     try Task.checkCancellation()
-    let metadataUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).json")!
+    let metadataUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part3/\(imageNumber).json")!
     let metadataUrlRequest = URLRequest(url: metadataUrl)
     let (metadataData, metadataResponse) = try await URLSession.shared.data(for: metadataUrlRequest)
     guard (metadataResponse as? HTTPURLResponse)?.statusCode == 200 else {
@@ -47,6 +47,9 @@ func downloadMetadata(imageNumber: Int) async throws -> ImageMetadata {
 
 /// Async let will allows us to execute code concurrently. We can use structure concurrency where there is no depedencies between tasks.
 /// Image and Metadata download task will begin execution on paralled and suspend the function on return statement
+/// Suppose downloadMetadata fails and downloadImage is downloading a big image
+/// since downloadMetadata failed, downloadImage will be marked as cancelled
+/// Marking a task as cancelled does not actually mean that the task is cancelled. Instead, it simply notifies the task that its results are no longer needed. All the child tasks and their descendants will be cancelled when their parent is cancelled.
 func downloadImageAndMetadata(imageNumber: Int) async throws -> DetailedImage {
     async let image = downloadImage(imageNumber: imageNumber)
     async let metadata = downloadMetadata(imageNumber: imageNumber)
@@ -56,29 +59,6 @@ func downloadImageAndMetadata(imageNumber: Int) async throws -> DetailedImage {
 Task {
     do {
         let detailedImage = try await downloadImageAndMetadata(imageNumber: 1)
-    } catch {
-        print(error)
-    }
-}
-
-
-/// Suppose downloadMetadata fails and downloadImage is downloading a big image
-/// since downloadMetadata failed, downloadImage will be marked as cancelled
-/// Marking a task as cancelled does not actually mean that the task is cancelled. Instead, it simply notifies the task that its results are no longer needed. All the child tasks and their descendants will be cancelled when their parent is cancelled.
-func downloadMultipleImagesWithMetadata(imageNumbers: Int...) async throws -> [DetailedImage] {
-    var detailedImages = [DetailedImage]()
-    
-    for imageNumber in imageNumbers {
-        async let detailedImage = downloadImageAndMetadata(imageNumber: imageNumber)
-        detailedImages.append(try await detailedImage)
-    }
-    return detailedImages
-}
-
-Task {
-    do {
-        let detailedImages = try await downloadMultipleImagesWithMetadata(imageNumbers: 1)
-        print(detailedImages)
     } catch {
         print(error)
     }
